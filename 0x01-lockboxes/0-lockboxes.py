5e196252052be948cf27287eec199f64c01f6407
obapythonaire <abdulahogundare@gmail.com>
2023-01-05 17:23:38 +0100

    adjusting lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-""" Defines lockboxes """
+'''LockBoxes Challenge'''
 
 
 def canUnlockAll(boxes):
-    """ Determines whether all boxes can be unlocked or not
-    Returns: True if all boxes can be unlocked  and
-             False if not
-             """
-    keys = [0]
-    for key in keys:
-        for val in boxes[key]:
-            if val not in keys:
-                keys.append(val)
-    if len(keys) == len(boxes):
-        return True
-    return False
+    '''determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
+
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
+
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
